897ef9a38c945aa6b1fb90187bd3c924be5aad0a
limhyejin <hejin8307@naver.com>
2022-04-22 02:27:17 +0900

    show video

---
+++ src/components/video_list/videoList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './videoList.module.css';
+import VideoItem from '../video_item/videoItem';
+
+const VideoList = (props) => (
+  <ul>
+    {props.videos.map((video) => (
+      <VideoItem key={video.id} video={video} />
+    ))}
+  </ul>
+);
+
+export default VideoList;
