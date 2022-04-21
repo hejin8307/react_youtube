2dbdbb4893148bb74fb5b4e7aea5021226d76a60
limhyejin <hejin8307@naver.com>
2022-04-22 02:28:11 +0900

    connect api

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './app.css';
+import Navbar from './components/navbar/navbar';
+import VideoList from './components/video_list/videoList';
 
 function App() {
-  return <h1>hello</h1>;
+  const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResult=25&key=${apiKey}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <VideoList videos={videos} />
+    </>
+  );
 }
 
 export default App;
